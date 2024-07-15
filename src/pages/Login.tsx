935e5e34a0295e66a67dc8f775cc08b614949b90
wild-mental <wild.mental@kidsnote.com>
2024-07-15 14:14:05 +0900

    [레이아웃][디자인] 로그인 페이지 반응형으로 변경 - 미디어 쿼리 추가 - 이미지에 포함된 폰트를 타이포그래피로 분리 - 우측 배경이미지 변경 및 작은 화면에서 상단 정렬 - baseUrl 설정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Grid, Typography, Link as MuiLink } from '@mui/material';
+import {Button, Grid, Typography, Link as MuiLink, useMediaQuery, Theme} from '@mui/material';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import CustomLoginTextField from "../components/CustomLoginTextField";
 
@@ -10,48 +10,56 @@ const Login: React.FC = () => {
         navigate('/main');  // 로그인 성공 후 메인 페이지로 리다이렉트
     };
 
+    const isSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+
     return (
         <Grid container sx={{ height: '100vh' }}>
             <Grid item xs={12} sm={6} sx={{ backgroundColor: '#152238', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color: 'white', padding: 3 }}>
                 <Typography variant="h4" component="h1" gutterBottom>
                     Log in to <span style={{ color: '#0026ED' }}>DeepTech</span>
                 </Typography>
-                {/*<Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}> /!* 추가된 Box 컨테이너 *!/*/}
-                {/*    <TextField*/}
-                {/*        label="User ID"*/}
-                {/*        variant="outlined"*/}
-                {/*        margin="normal"*/}
-                {/*        InputLabelProps={{ style: { color: 'white', fontSize: '25px' } }}*/}
-                {/*        inputProps={{ style: { color: 'white', fontSize: '20px' } }}*/}
-                {/*        sx={{ "& .MuiOutlinedInput-root": { height: '55px', width: '500px' }, "& .MuiInputLabel-root": { lineHeight: 1.5 } }}*/}
-                {/*    />*/}
-                {/*</Box>*/}
-                {/*<Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>*/}
-                {/*    <TextField*/}
-                {/*        label="Password"*/}
-                {/*        type="password"*/}
-                {/*        variant="outlined"*/}
-                {/*        margin="normal"*/}
-                {/*        InputLabelProps={{ style: { color: 'white', fontSize: '25px' } }}*/}
-                {/*        inputProps={{ style: { color: 'black', fontSize: '20px' } }}*/}
-                {/*        sx={{ "& .MuiOutlinedInput-root": { height: '55px', width: '500px' }, "& .MuiInputLabel-root": { lineHeight: 1.5 } }}*/}
-                {/*    />*/}
-                {/*</Box>*/}
                 <CustomLoginTextField label="User ID" />
                 <CustomLoginTextField label="Password" type="password" />
                 <MuiLink component={RouterLink} to="/password-reset" underline="hover" sx={{ mt: 1 }}>
                     Forgot your password?
                 </MuiLink>
-                <Button variant="contained" color="primary" fullWidth onClick={handleLogin} sx={{ marginTop: 2, width : '500px',height: '50px' }}>
+                <Button variant="contained" color="primary" fullWidth onClick={handleLogin} sx={{ marginTop: 2, maxWidth: '500px', height: '50px' }}>
                     LOG IN
                 </Button>
                 <MuiLink component={RouterLink} to="/register" underline="hover" sx={{ mt: 2 }}>
                     Need an account? Register here
                 </MuiLink>
             </Grid>
-            {/* 오른쪽 배경 이미지 부분 */}
-            <Grid item xs={12} sm={6} sx={{ background: 'url(login-background.png) no-repeat center center', backgroundSize: 'cover', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 3 }}>
-                {/* 내용 없음 */}
+            <Grid item xs={12} sm={6} sx={{
+                background: 'url(login-back.jpeg) no-repeat center center',
+                backgroundSize: 'cover',
+                display: 'flex',
+                flexDirection: isSmallScreen ? 'column' : 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: 3,
+                order: isSmallScreen ? -1 : 0,
+            }}>
+                <Typography variant="h4" component="h1" gutterBottom sx={{
+                    textAlign: 'center',
+                    color: 'rgba(0, 0, 139, 1.0)', /* Dark Blue */
+                    'text-shadow':
+                        '2px 2px 2px rgba(144, 144, 238, 0.8),' + /* Light Blue */
+                        '4px 4px 10px rgba(144, 144, 238, 0.6)'
+                }}>
+                    Here we can provide
+                </Typography>
+                <Typography variant="h6" component="p" sx={{
+                    textAlign: 'center',
+                    color: 'rgba(0, 0, 100, 1.0)', /* Dark Blur */
+                    'text-shadow':
+                        '2px 2px 2px rgba(144, 144, 238, 0.8),' + /* Light Blue */
+                        '4px 4px 10px rgba(144, 144, 238, 0.6)'
+                }}>
+                    ● Various sort of it news<br />
+                    ● Saving Articles<br />
+                    ● get email of your favorite domain
+                </Typography>
             </Grid>
         </Grid>
     );
