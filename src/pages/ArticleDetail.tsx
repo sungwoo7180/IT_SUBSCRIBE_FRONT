de973455a2551f780f49df808e60047fc7dca3aa
HyojinBae <hyojin9180@gmail.com>
2024-08-26 11:39:44 +0900

    로그용 API를 위한 변경

--- src/pages/ArticleDetail.tsx
+++ src/pages/ArticleDetail.tsx
@@ -23,7 +23,7 @@ const ArticleDetail: React.FC = () => {
     useEffect(() => {
         const fetchArticle = async () => {
             try {
-                await axiosInstance.get(`/article/article/${articleId}`);
+                await axiosInstance.post(`/article/views/${articleId}`);
                 // const response = await axiosInstance.get(`/article/article/${articleId}`);
                 //setArticle(response.data);
             } catch (err) {
